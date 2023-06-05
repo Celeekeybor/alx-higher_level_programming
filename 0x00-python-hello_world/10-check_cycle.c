9d599bf8b973867ef9d00b932b15ea4cba1ef334
Celeekeybor <celestinbor02@gmail.com>
2023-06-05 13:39:01 -0700

    solve it

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,23 +1,26 @@
 #include "lists.h"
 
 /**
- * check_cycle - cycle check
- * @list: check list
+ * check_cycle - checks cycle
+ * @list: linked lists to check
  *
  * Return: true or false
  */
 int check_cycle(listint_t *list)
 {
-listint_t *slow = list;
-listint_t *fast = list;
-if (!list)
-return (0);
-while (slow && fast && fast->next)
-{
-slow = slow->next;
-fast = fast->next->next;
-if (slow == fast)
-return (1);
-}
-return (0);
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	if (!list)
+		return (0);
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
 }
