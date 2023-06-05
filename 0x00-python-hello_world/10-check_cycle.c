a3b9f8c951833daf1297cccffeb1ead3ea941dca
Celeekeybor <celestinbor02@gmail.com>
2023-06-05 15:19:17 -0700

    linked list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * check_cycle -  checks if a singly linked list has a cycle in it.
+ * check_cycle -  checks if a singly linked list has a cycle in it
  * @list: pointer to the node
  * Return: true or false
  */
