73f8074817bf7a4c7b771c211f1a6d9bbe931bd1
Celeekeybor <celestinbor02@gmail.com>
2023-06-05 13:32:28 -0700

    check cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check cycle
+ * @list: check list
+ *
+ * Return: zero
+ */
+int check_cycle(listint_t *list)
+{
+listint_t *slow = list;
+listint_t *fast = list;
+if (!list)
+return (0);
+while (slow && fast && fast->next)
+{
+slow = slow->next;
+fast = fast->next->next;
+if (slow == fast)
+return (1);
+}
+return (0);
+}
