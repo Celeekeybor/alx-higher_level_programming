dade2188c77931c11bd2591501ec1fa4b830e434
Celeekeybor <celestinbor02@gmail.com>
2023-06-08 14:54:45 -0700

    infinitely add them

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ import sys
 
 if __name__ == "__main__":
     
-    summation = 0
+    result = 0
     for num in range(len(sys.argv) - 1):
-        summation += int(sys.argv[i + 1])
-    print("{}".format(summation))
+        result += int(sys.argv[i + 1])
+    print("{}".format(result))
