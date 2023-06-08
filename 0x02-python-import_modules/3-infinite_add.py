e72fa4c9a91c17b7a8f8a891a866e5e6aad17699
Celeekeybor <celestinbor02@gmail.com>
2023-06-08 14:49:21 -0700

    add ifinitely

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+import sys
 
 if __name__ == "__main__":
-    import sys
-
+    
     summation = 0
     for num in range(len(sys.argv) - 1):
         summation += int(sys.argv[i + 1])
