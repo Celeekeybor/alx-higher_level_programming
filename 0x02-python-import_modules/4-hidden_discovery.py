edabe4d3849e1b98a0fdde02f442a768c6b35e83
Celeekeybor <celestinbor02@gmail.com>
2023-06-08 15:25:47 -0700

    hiddden names

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     import hidden_4
 
     names = dir(hidden_4)
-    if name in names:
-        else name[:2] != "__":
+    for name in names:
+        if name[:2] != "__":
             print(name)
