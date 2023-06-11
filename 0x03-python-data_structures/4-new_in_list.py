2faa7d38d45f5c48b13c1f802c382e172af32a18
Celeekeybor <celestinbor02@gmail.com>
2023-06-11 15:44:57 -0700

    new in the list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list[:]
+    _list = my_list[:]
+    _list[idx] = element
+    return _list
