762bc65a320d6500caad4f87365cbf731aba4591
Celeekeybor <celestinbor02@gmail.com>
2023-06-15 06:36:58 -0700

    matrix 2by2

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
 
-    for num in range(len(matrix)):
-        new_matrix[num] = list(map(lambda x: x**2, matrix[num]))
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
 
     return (new_matrix)
