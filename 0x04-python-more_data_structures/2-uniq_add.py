9db10688f59cec5e0ad2ec64695cf247caf8c7a3
Celeekeybor <celestinbor02@gmail.com>
2023-06-15 07:59:01 -0700

    unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     uniq_list = set(my_list)
-    val = 0
+    num = 0
 
-    for num in uniq_list:
-        val += i
+    for val in uniq_list:
+        num += val
 
-    return (val)
+    return (num)
