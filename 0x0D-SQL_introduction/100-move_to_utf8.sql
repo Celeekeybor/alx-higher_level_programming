97c7f62c66b4343b96d7db0fbbbe76ff33a97f1c
Celeekeybor <celestinbor02@gmail.com>
2023-08-15 14:12:38 -0700

    alter table

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,6 @@
+ALTER DATABASE hbtn_0c_0
+    CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Changes the properties of a table in the database
+USE hbtn_0c_0;
+ALTER TABLE first_table
+    CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
