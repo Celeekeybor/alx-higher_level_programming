dfc6fba42592b85a2c45416761a7d2fce16cd551
Celeekeybor <celestinbor02@gmail.com>
2023-08-17 15:02:45 -0700

    count shows

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- lists all shows contained in hbtn_0d_tvshows without a genre linked
+SELECT title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY title, tv_show_genres.genre_id;
