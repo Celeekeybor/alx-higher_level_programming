d40d6d32e49d5138f998d611af5aa43158f3fbcc
Celeekeybor <celestinbor02@gmail.com>
2023-08-17 14:34:18 -0700

    give permission

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
--- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on
--- your server (in localhost).
-CREATE USER 'user_0d_2'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+-- list all privileges of MySQL users `user_0d_1` and `user_0d_2`
+SHOW GRANTS FOR `user_0d_1`@`localhost`;
+SHOW GRANTS FOR `user_0d_2`@`localhost`;
