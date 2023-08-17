2366435905d01fdc15033cbf3dfe2421911d180a
Celeekeybor <celestinbor02@gmail.com>
2023-08-17 14:41:57 -0700

    privilege

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- list all privileges of MySQL users `user_0d_1` and `user_0d_2`
-SHOW GRANTS FOR `user_0d_1`@`localhost`;
-SHOW GRANTS FOR `user_0d_2`@`localhost`;
+echo "CREATE USER 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
+echo "GRANT ALL PRIVILEGES ON . TO 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
