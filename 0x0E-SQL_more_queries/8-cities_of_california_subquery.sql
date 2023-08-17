fcdbb609077cc05c4c1ecbc78eb36ad3784b3681
Celeekeybor <celestinbor02@gmail.com>
2023-08-17 14:50:22 -0700

    california

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- lists all the cities of California in the database hbtn_0d_usa
+SELECT id, name FROM cities
+WHERE state_id = (
+      SELECT id FROM states
+      WHERE name = "California")
+ORDER BY id;
