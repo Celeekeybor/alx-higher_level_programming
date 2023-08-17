e33c0c81faa69842955607a8d14029f7e9c4d255
Celeekeybor <celestinbor02@gmail.com>
2023-08-17 14:27:33 -0700

    privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++
@@ -1,4 +0,0 @@
--- script 1
--- creates the MySQL server user user_0d_1.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
