149dbc081a00d9979a635cf4e4d85d2d3de371af
Celeekeybor <celestinbor02@gmail.com>
2023-09-12 13:43:20 -0700

    add Js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
 function add (a, b) {
-    const c = a + b;
-    console.log(c);
-  }
-  
-  add(Number(process.argv[2]), Number(process.argv[3]));
+  const res = a + b;
+  return res;
+}
+
+console.log(add(a, b));
