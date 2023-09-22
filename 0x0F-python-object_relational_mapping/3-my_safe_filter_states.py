2c99c1079bb89f71dc0f6e2cf9843f92b9bee1df
Celeekeybor <celestinbor02@gmail.com>
2023-09-22 07:07:09 -0700

    my safe filter state

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,38 +1,73 @@
 #!/usr/bin/python3
-"""Filter states by user input"""
+
+"""
+This script lists all values in the `states` table of the
+`hbtn_0e_0_usa` database
+where `name` matches the argument `state name searched`.
+
+The script takes four arguments:
+    * The MySQL username
+    * The MySQL password
+    * The database name
+    * The state name to search for
+
+The script connects to the MySQL database on the
+default host (localhost) and port (3306).
+
+The script uses the `sys` module to get the MySQL username, password,
+and database name from the command line arguments.
+
+The script uses the `MySQLdb` module to execute the SQL query `SELECT *
+FROM states WHERE name = %s ORDER BY id`.
+
+The results of the query are then printed to the console.
+"""
+
+
+import sys
 import MySQLdb
 
 
 def main():
+    """
+    This function connects to the MySQL database and lists all values in the
+    states table
+    where name matches the argument.
+    """
 
-    if len(argv) == 5:
-        # connect
-        db = MySQLdb.connect(host='localhost',
-                             port=3306,
-                             user=argv[1],
-                             passwd=argv[2],
-                             db=argv[3])
-        # cursor
-        c = db.cursor()
+    # Get the MySQL username from the command line arguments.
+    username = sys.argv[1]
 
-        # execute query
-        c.execute("SELECT * FROM states WHERE name = %s\
-        ORDER BY states.id ASC", (argv[4], ))
+    # Get the MySQL password from the command line arguments.
+    password = sys.argv[2]
 
-        # fetch
-        rows = c.fetchall()
+    # Get the database name from the command line arguments.
+    database = sys.argv[3]
 
-        # print
-        for row in rows:
-            print(row)
+    # Get the state name to search for from the command line arguments.
+    city_name = sys.argv[4]
 
-        # close
-        c.close()
-        db.close()
-    else:
-        return
+    # Connect to the MySQL database.
+    connection = MySQLdb.connect(user=username, passwd=password, db=database)
+
+    # Create a cursor object.
+    cursor = connection.cursor()
+
+    # Execute the SQL query.
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id"
+    cursor.execute(query, (city_name,))
+
+    # Iterate over the results and print each row.
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close the cursor object.
+    cursor.close()
+    # Close the connection to the database.
+    connection.close()
+
 
 if __name__ == "__main__":
-    from sys import argv
     main()
 
