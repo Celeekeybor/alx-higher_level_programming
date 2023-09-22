c04b0b6b94c1857033926401c32df05b01aba0d0
Celeekeybor <celestinbor02@gmail.com>
2023-09-22 06:15:18 -0700

    filter all cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,18 +1,46 @@
 #!/usr/bin/python3
-"""Script that takes in the name of a state as an argument and lists
-all cities of that state, using the database hbtn_0e_4_usa"""
+"""
+Script that lists all `cities` in the `cities` table of `hbtn_0e_4_usa`
+where the city's state matches the argument `state name`.
+
+Arguments:
+    mysql username (str)
+    mysql password (str)
+    database name (str)
+    state name (str)
+"""
+
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities \
-    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
-    ORDER BY cities.id", (argv[4],))
-    rows = cur.fetchall()
-    print(", ".join(city[0] for city in rows))
-    cur.close()
-    db.close()
+    # Check if all four arguments are provided
+    if len(sys.argv) != 5:
+        print("Usage: {} <mysql username> <mysql password>\
+        <database name> <state name>".format(sys.argv[0]))
+        sys.exit(1)
 
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+
+    # Connect to the MySQL server
+    db = MySQLdb.connect(user=username, passwd=password, db=database)
+    cursor = db.cursor()
+
+    # Create and execute the query to fetch cities for the given state
+    query = "SELECT name FROM cities WHERE state_id IN\
+    (SELECT id FROM states WHERE name = %s) ORDER BY id"
+    cursor.execute(query, (state,))
+
+    # Fetch all the results
+    results = cursor.fetchall()
+
+    # Display the results
+    cities = [row[0] for row in results]
+    print(", ".join(cities))
+
+    # Close the cursor and connection
+    cursor.close()
+    db.close()
