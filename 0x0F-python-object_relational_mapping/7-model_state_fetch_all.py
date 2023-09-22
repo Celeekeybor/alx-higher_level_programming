deb6286bb167567e4f7c1f62a5aa762f8303a595
Celeekeybor <celestinbor02@gmail.com>
2023-09-22 07:27:35 -0700

    model state fetch all

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
-"""Lists all the states"""
-from sys import argv
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+"""
+Script that lists all State objects from the database hbtn_0e_6_usa.
+"""
+
+import sys
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
 
 if __name__ == "__main__":
-    db = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]),
-        pool_pre_ping=True)
-    Base.metadata.create_all(db)
-    # Create a premade "Session" class
-    Session = sessionmaker(bind=db)
-    # Instance of the Session
-    ses = Session()
-    # Make a query
-    res = ses.query(State).order_by(State.id).all()
-    for item in res:
-        print('{}: {}'.format(item.id, item.name))
-    # Close the instance of session
-    ses.close()
+    # Get MySQL credentials and database name from command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
+    # Connect to MySQL server
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}'
+    )
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session instance
+    session = Session()
+
+    # Query all State objects and sort them by id in ascending order
+    states = session.query(State).order_by(State.id).all()
+
+    # Print the retrieved State objects
+    for state in states:
+        print(f"{state.id}: {state.name}")
