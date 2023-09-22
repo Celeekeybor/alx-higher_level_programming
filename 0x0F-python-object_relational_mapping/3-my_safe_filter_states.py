82fd002e23f12abb0c89c67f5b36940121d698e4
Celeekeybor <celestinbor02@gmail.com>
2023-09-22 06:02:12 -0700

    updated styling guideline

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,18 +1,38 @@
 #!/usr/bin/python3
-"""Script that takes in arguments and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument, safe from 
-injections"""
+"""Filter states by user input"""
 import MySQLdb
-from sys import argv
+
+
+def main():
+
+    if len(argv) == 5:
+        # connect
+        db = MySQLdb.connect(host='localhost',
+                             port=3306,
+                             user=argv[1],
+                             passwd=argv[2],
+                             db=argv[3])
+        # cursor
+        c = db.cursor()
+
+        # execute query
+        c.execute("SELECT * FROM states WHERE name = %s\
+        ORDER BY states.id ASC", (argv[4], ))
+
+        # fetch
+        rows = c.fetchall()
+
+        # print
+        for row in rows:
+            print(row)
+
+        # close
+        c.close()
+        db.close()
+    else:
+        return
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
-                (argv[4],))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    from sys import argv
+    main()
+
