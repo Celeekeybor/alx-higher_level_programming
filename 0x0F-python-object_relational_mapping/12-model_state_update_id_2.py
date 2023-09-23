5b9fc6f8c9773c7685022fd4c37441ff06b78007
Celeekeybor <celestinbor02@gmail.com>
2023-09-23 08:41:24 -0700

    12 model state update object state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+changes the name of a State object from the database hbtn_0e_6_usa
+"""
+from sys import argv
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    for state in session.query(State).filter_by(id=2).order_by(State.id).all():
+        state.name = "New Mexico"
+    session.commit()
+    session.close()
