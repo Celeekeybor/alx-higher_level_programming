25caa30d2f70a120a4a5002d3238863e209af800
Celeekeybor <celestinbor02@gmail.com>
2023-09-30 11:29:26 -0700

    my github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python3
-"""takes your Github credentials 
+"""
+a Python script that takes your GitHub credentials
 """
 import requests
-from sys import argv
+import sys
 
-if __name__ == "__main__":
+if len(sys.argv) != 3:
+    print("Usage: ./10-my_github.py <username> <access_token>")
+    sys.exit(1)
 
-    username = argv[1]
-    password = argv[2]
-    try:
-        r = requests.get('https://api.github.com/user', auth=(
-            argv[1], argv[2])).json()
-        print(r.get('id'))
-    except:
-        print("None")
+username = sys.argv[1]
+access_token = sys.argv[2]
+
+# Construct the URL to fetch your GitHub user information
+url = f"https://api.github.com/user"
+
+# Set up the Basic Authentication headers using your username and access token
+headers = {
+    "Authorization": f"Basic {username}:{access_token}"
+}
+
+try:
+    # Send a GET request to the GitHub API
+    response = requests.get(url, headers=headers)
+    # Check if the request was successful
+    if response.status_code == 200:
+        user_info = response.json()
+        user_id = user_info.get('id')
+        print(user_id)
+    else:
+        print(None)
+except requests.exceptions.RequestException as e:
+    # Handle any request-related errors and print an error message
+    print(None)
