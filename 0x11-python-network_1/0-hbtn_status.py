1461af1d54364a0069502df1fae6be92a0687565
Celestine Chepkobil <122092941+Celeekeybor@users.noreply.github.com>
2023-09-29 19:32:23 -0700

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""Fetch
-https://intranet.hbtn.io/status
-using urlib paage
+"""Fetches the URL: https://intranet.hbtn.io/status
 """
 
-import urllib.request
+from urllib.request import Request, urlopen
 
-if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
-        response = request.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode('UTF-8')))
+
+if __name__ == "__main__":
+    req = Request('https://intranet.hbtn.io/status')
+
+    with urlopen(req) as res:
+        content = res.read()
+        utf8_content = content.decode('utf-8')
+
+        print('Body response:')
+        print('\t- type: {_type}'.format(_type=type(content)))
+        print('\t- content: {_content}'.format(_content=content))
+        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
